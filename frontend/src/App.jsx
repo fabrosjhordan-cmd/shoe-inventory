ef4867362c5d37ff30da086596069e31ebd718ce
fabrosjhordan-cmd <fabrosjhordan@gmail.com>
2025-11-06 13:25:16 +0800

    Additional: React Icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,12 @@ import SideBar from '../components/SideBar';
 
 function App() {
   const [data, setData] = useState([]);
-  useEffect(()=>{
-    fetch('http://localhost:8081/users')
-    .then(res => res.json())
-    .then(data =>setData(data))
-    .catch(err => console.log(err));
-  }, [])
+  // useEffect(()=>{
+  //   fetch('http://localhost:8081/users')
+  //   .then(res => res.json())
+  //   .then(data =>setData(data))
+  //   .catch(err => console.log(err));
+  // }, [])
   return (
   <div>
     <SideBar />
